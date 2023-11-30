fb8849e413e1f28eb6d93b8a9343fccfbfc5a3ff
fred eseka <fred.eseka@gmail.com>
2023-11-30 07:57:12 +0100

    COMMIT

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-num = float(input("Enter a number: "))
-if num > 0:
+number = float(input("Enter a number: "))
+if number > 0:
    print("Positive number")
-elif num == 0:
+elif number == 0:
    print("Zero")
 else:
    print("Negative number")
