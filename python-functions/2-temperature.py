a0026cd568330a860a7b6291f3d234decba777c8
fred eseka <fred.eseka@gmail.com>
2023-11-30 17:51:07 +0100

    commit

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,5 @@
 def reverse_string(string):
+
     reversed_string = string[::-1]
+    
     return reversed_string
--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,6 +1,9 @@
 def fibonacci_sequence(n):
     fibonacci = [0, 1]
+    
     while len(fibonacci) < n:
+            
             anothernum = fibonacci[-1] + fibonacci[-2]
             fibonacci = fibonacci + [anothernum]
+
     return fibonacci[:n]
