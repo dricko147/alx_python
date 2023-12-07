5e78f695e79f790bc7d723d2e8bed44916fb5f08
fred eseka <fred.eseka@gmail.com>
2023-12-07 19:32:47 +0100

    commit

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+# update_dictionary.py
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return (a_dictionary)
--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# best_score.py
+
 def best_score(a_dictionary):
     if a_dictionary:
         best_key = max(a_dictionary, key=lambda k: a_dictionary[k])
