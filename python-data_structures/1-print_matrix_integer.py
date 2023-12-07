028aa220e9e4cc0a96c919bb03f4561df604a9aa
fred eseka <fred.eseka@gmail.com>
2023-12-07 14:54:30 +0100

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,20 @@
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j, num in enumerate(i):
-            if j == len(i) - 1:
-                print("{:d}".format(num), end="")
-            else:
-                print("{:d} ".format(num), end="")
-    print()
+    def print_matrix_with_indices(matrix):
+    # Loop over each row
+      for i in range(len(matrix)):
+        # Loop over each column in the current row
+        for j in range(len(matrix[i])):
+            # Print element at row i, column j
+            print(matrix[i][j], end=' ')
+        # Print a new line after each row
+        print()
+ 
+ 
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# Test the function with our matrix
+print(matrix)
+ 
