63d4c499e2969d3e060746a2b9d06685caa813a0
fred eseka <fred.eseka@gmail.com>
2023-12-07 15:08:36 +0100

    commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -6,12 +6,6 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(num), end="")
             else:
                 print("{:d} ".format(num), end="")
-                
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 # Test the function with our matrix
-print(matrix)
+    print()
  
