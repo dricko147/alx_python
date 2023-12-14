0e2109ab13f46c36213e3dda287cf3fc064b76e8
fred eseka <fred.eseka@gmail.com>
2023-12-14 19:06:09 +0100

    final commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
 # 4-square.py
 # dricko147
-"""Define a class Square."""
+"""Define a Class Square."""
 
-
-class Square:
-   def __init__(self, size=0):
-        self.size = size @property
-
-def size(self):
-        return self.__size 
+class Square():
+    """Define a private attribute size"""
+    def __init__(self, size=0):
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     
-@size.setter
-def size(self, value):
+
+    """getter method that return the size"""
+    @property
+    def size(self):
+        return self.__size
+    
+
+    """"setter method that set a new value"""
+    @size.setter
+    def size(self, value):
         if not isinstance(value,int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-            
-def area(self):
-        return self.__size ** 2
+        self.__size = value
+
+
+    """Define instance method area"""
+    def area(self):
+        return self.__size * self.__size
     
-def my_print(self):
+
+    """Define my print method that prints in stdout the square with the character '#' """
+    def my_print(self):
         if self.__size == 0:
-            print()
-        else:
+            print("")
+        for _ in range(self.__size):
             for _ in range(self.__size):
-                print("#" * self.__size)
-                
-if __name__=="__main__":
-    my_square = Square(3)
-    my_square.my_print()
-
-    print("--")
-
-    my_square.size = 10
-    my_square.my_print()
-
-    print("--")
-
-    my_square.size = 0
-    my_square.my_print()
-
-    print("--")
+                print("#", end="")
+            print()
