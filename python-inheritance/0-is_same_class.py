9014c817d6198f0684f573c3f9aea146f04f375c
fred eseka <fred.eseka@gmail.com>
2023-12-14 22:26:09 +0100

    final commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,5 +2,6 @@
 # 0-is_same_class.py
 # dricko147
 '''Define 'is same class' Function'''
-def is_same_class(obj, a_class):  
-      return isinstance(obj, a_class)
+def is_same_class(obj, a_class):
+    '''to check if its true or false'''
+    return type(obj) == a_class
--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -2,8 +2,8 @@
 # # 1-is_kind_of_class.py
 # dricko147
 '''functions that inherits from'''
-def inherits_from(obj, a_class):
-    '''functions that check if its true it inherits from the type object'''
-    if type(obj) is a_class:
-        return False
-    return issubclass(type(obj), a_class)
+def is_kind_of_class(obj, a_class):
+    """Check  if the object is an instance of, 
+    or if the object is an instance 
+    of a class that inherited from, the specified class"""
+    return isinstance(obj,a_class)
