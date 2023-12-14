a79fa940253e58eaebb037f1b532688558dc046e
fred eseka <fred.eseka@gmail.com>
2023-12-14 19:20:02 +0100

    SQUARE

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 # 0-square.py
 # dricko147
-class Square:
-    '''initializing size'''
+"""Define a Square Class"""
+class Square():
+    """Define a private attribute size"""
     def __init__(self, size):
         self.__size = size
-    
-if __name__=="__main__":
-    '''this is a module to test the class'''
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-
-    try:
-        print(my_square.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square.__size)
-    except Exception as e:
-        print(e)
